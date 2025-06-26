0e52b5316fec5faede714330bb45daba04a57f9e
Arthur Sena <122495363+HelpSeller@users.noreply.github.com>
2025-06-26 11:41:49 -0300

    Atualização vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // …outras configs…
+  preview: {
+    host: '0.0.0.0',          // já necessário para o Render
+    port: Number(process.env.PORT) || 4173,
+    allowedHosts: ['dashv3.onrender.com']
+  }
 })
